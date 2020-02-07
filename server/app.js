f2d38b1d37d150c076a9c50e1ead71f3459a3d83
Khinshan Khan <khanshan501@gmail.com>
2020-02-07 17:15:22 -0500

    server now utilizes .env files

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+require("dotenv").config();
 
 var usersRouter = require("./routes/users");
 
