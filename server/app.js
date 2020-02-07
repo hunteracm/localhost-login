08623d12f0eb7d9e6dc1332fbe54ea7dcb3d1078
Khinshan Khan <khanshan501@gmail.com>
2020-02-07 18:11:17 -0500

    mongodb atlas uri using .env variables

--- server/app.js
+++ server/app.js
@@ -1,17 +1,28 @@
-var express = require("express");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var usersRouter = require("./routes/users");
+const dotenv = require("dotenv");
+const dotenvExpand = require("dotenv-expand");
 
-var app = express();
+const myEnv = dotenv.config();
+dotenvExpand(myEnv);
+
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0-eprga.gcp.mongodb.net/test?retryWrites=true&w=majority`;
+
+const addRouter = require("./routes/add");
+const getRouter = require("./routes/get");
+
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/users", usersRouter);
+app.use("/add", addRouter);
+app.use("/get", addRouter);
 
 module.exports = app;
