1eb20878d1c063591ef760df31071e5aef62b988
Khinshan Khan <khanshan501@gmail.com>
2020-02-07 21:07:19 -0500

    mongoose database initial connection

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const express = require("express");
 const logger = require("morgan");
 const helmet = require("helmet");
+const mongoose = require("mongoose");
 
 // env variables
 const dotenv = require("dotenv");
@@ -21,50 +22,49 @@ const dbRouter = require("./routes/db");
 // instantiate our express application
 const app = express();
 
-// a helper function to create our app with configurations and middleware
-const configureApp = () => {
-  // express middleware
-  app.use(helmet());
-  app.use(logger("dev"));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(compression());
-  app.use(cookieParser());
-
-  app.use(
-    cors({
-      origin: "*",
-    })
-  );
-
-  // Mount our db router
-  app.use("/db", dbRouter);
-
-  // error handling
-  app.use((req, res, next) => {
-    if (path.extname(req.path).length) {
-      const err = new Error("Not found");
-      err.status = 404;
-      next(err);
-    } else {
-      next();
-    }
+// connect to our mongodb atlas instance
+mongoose
+  .connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() =>
+    console.log("MongoDB database connection established successfully!")
+  )
+  .catch(err => {
+    console.log(`DB Connection Error: ${err.message}`);
   });
 
-  // more error handling
-  app.use((err, req, res, next) => {
-    console.error(err);
-    console.error(err.stack);
-    res.status(err.status || 500).send(err.message || "Internal server error.");
-  });
-};
+// express middleware
+app.use(helmet());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(compression());
+app.use(cookieParser());
 
-// main function declaration
-const bootApp = async () => {
-  await configureApp();
-};
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-// main function invocation
-bootApp();
+// Mount our db router
+app.use("/db", dbRouter);
+
+// error handling
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error("Not found");
+    err.status = 404;
+    next(err);
+  } else {
+    next();
+  }
+});
+
+// more error handling
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error.");
+});
 
 module.exports = app;
