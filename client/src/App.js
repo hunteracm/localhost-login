63108ed355241d310e3ce3d75c39d3ee227bc7e7
Fioringo <shtuk.andrey@gmail.com>
2020-02-13 23:26:36 -0500

    updated app to have routes and defined routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CheckIn from "./components/checkIn";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <div className="navBar">
+          <span>
+            {/* <img className="logo" src=""/> */}
+            <Link to="/" className="logo">
+              Hunter ACM Chapter
+            </Link>
+          </span>
+          <span>
+            <Link to="/" className="navElement">
+              Home
+            </Link>
+          </span>
+          <span>
+            <Link to="/checkIn" className="navElement">
+              Check In
+            </Link>
+          </span>
+          <span>
+            <Link to="/calendar" className="navElement">
+              Calendar
+            </Link>
+          </span>
+          <span>
+            <Link to="/calendar" className="navElement">
+              About Us
+            </Link>
+          </span>
+        </div>
+      </nav>
+      <Switch>
+        <Route path="/checkIn">
+          <CheckIn />
+        </Route>
+        <Route path="/calendar">{/* Calendar */}</Route>
+        <Route path="/">
+          {/* Home */}
+          <div style={{ textAlign: "center" }}>Home</div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
